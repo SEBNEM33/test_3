154af8a33d1308893182fb422141d1e3d0ee1240
sebnem33 <sebnemece33@gmail.com>
2024-03-06 14:18:50 -0500

    0603

---
+++ modul_1/python_module/package_1/my_module.py
@@ -0,0 +1,32 @@
+def my_factorial(x):
+
+    """Bu fonksiyon girilen sayının factorial değerini döndürür."""
+
+    if x >= 0:
+        sonuç = 1
+        for i in range(1, x + 1):
+            sonuç *= i
+
+    return sonuç
+
+def my_len(x):
+
+
+    counter = 0
+
+    for i in x:
+        counter += 1
+
+    return counter
+
+def my_int_len(x):
+
+
+    counter = 0
+
+    for i in str(x):
+        counter += 1
+
+    return counter
+
+pi = 3.14
