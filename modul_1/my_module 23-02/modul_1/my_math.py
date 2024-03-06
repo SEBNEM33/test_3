868f38df03cddb82f34f60067d8bc33c3bc35988
sebnem33 <sebnemece33@gmail.com>
2024-03-06 14:23:26 -0500

    0603-2

---
+++ modul_1/my_module 23-02/modul_1/my_math.py
@@ -0,0 +1,15 @@
+def my_pow(x,y):
+
+    """x in y kuvvetini alır"""
+    return x ** y
+
+my_pi = 3.14
+
+def tek_çift(x):
+
+    """tek sayının karesini .ift sayının kendisini döndürür"""
+
+    return x ** 2 if x % 2 == 1 else x
+
+
+
